befdcd8d6e7a7dd3c8c10ee5ce03ed5f88d178f5
Subham kumar <110382359+gamingyard7016@users.noreply.github.com>
2026-09-19 04:21:18 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Vowel or Not/vowel-or-not.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    char ch;
+    cin >> ch;
+
+    // code here
+    if(ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U' || ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') cout<<"true";
+    else cout<<"false";
+
+    return 0;
+}
