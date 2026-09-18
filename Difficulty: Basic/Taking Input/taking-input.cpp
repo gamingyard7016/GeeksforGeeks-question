07b5e0e9290f5994c8ec7119c4c6e72be14bd6a6
Subham kumar <110382359+gamingyard7016@users.noreply.github.com>
2026-09-19 03:03:34 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Taking Input/taking-input.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string s;
+    int n;
+    float f;
+    int ff; // To Store floor of float variable f
+
+    // code here
+    ff = f;
+    getline(cin, s);
+    cin>>n;
+    cin>>ff;
+    // cin>>ff;
+
+    cout << s << endl;
+    cout << n << endl;
+    cout << ff << endl;
+    return 0;
+}
