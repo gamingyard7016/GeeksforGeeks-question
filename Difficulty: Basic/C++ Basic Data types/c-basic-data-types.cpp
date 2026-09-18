701682e038aa7c8834fc2be92b36896c1a3a8d4a
Subham kumar <110382359+gamingyard7016@users.noreply.github.com>
2026-09-19 05:17:02 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/C++ Basic Data types/c-basic-data-types.cpp
@@ -0,0 +1,10 @@
+// User functiom template for C++
+
+class Solution {
+	public:
+	int BasicDataType(string s) {
+		const size_t i { s.find('.')};
+		return s.size() == 1 && !isdigit(s.front())
+		? 1 : i == -1 || s.size() - i < 7 ? 4 : 8;
+	} // 0.17+
+};
